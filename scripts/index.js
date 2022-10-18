2aed745e91f2a7c143e7140fac3f4c3f29dc2e99
joaobuga35 <joaobuga35@gmail.com>
2022-10-17 22:50:52 -0300

    Update about last users

--- scripts/index.js
+++ scripts/index.js
@@ -16,16 +16,36 @@ async function genericForm(){
     
         const valueAPI = await profileAPI(nameUser)
         console.log(valueAPI)
+
         objectsProfile.push(valueAPI.avatar_url)
         console.log(objectsProfile)
+        const newObjectJson = JSON.stringify(objectsProfile)
+        localStorage.setItem('@renderimg',newObjectJson)
+
         const stringTransform = JSON.stringify(valueAPI)
         if (!valueAPI.message) {
             localStorage.setItem('renderdados',stringTransform)
         }
     })
 }
+
 genericForm()
 
-// function renderImgProfile() {
-    
-// }
+const datasUser = localStorage.getItem('@renderimg')
+const newDatasUser = JSON.parse(datasUser)
+
+ function renderImgProfile(userProfile) {
+    const listProfiles = document.querySelector('#list-profile-add')
+
+    userProfile.forEach(element => {
+        const li = document.createElement('li')
+        const img = document.createElement('img')
+
+        img.src = element
+        img.classList = 'img-profile-add'
+
+        li.append(img)
+        listProfiles.append(li)
+    });
+}
+renderImgProfile(newDatasUser)
